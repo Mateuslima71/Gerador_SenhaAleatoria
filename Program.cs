6e54a3213ce9cc9b9f955e0a7e03d6e133c6c864
Mateus Marcelino lima <mlima@narwalsistemas.com.br>
2024-04-24 21:05:03 -0300

    Esse é o meu gerador de senha.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int length = 10;
+
+            string randowchars = GenerateRandomChars(length);
+
+            Console.WriteLine("Caracteres aleatorio: " + randowchars);
+            Console.ReadLine();
+
+        }
+
+        static string GenerateRandomChars(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]|;:<>,.?/~";
+            Random random = new Random();
+            char[] result = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = chars[random.Next(chars.Length)];
+            }
+            return new string(result);
+            
+        }
+
+        
+    }
+}   
